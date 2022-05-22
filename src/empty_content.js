da47b6cb51020d0eb140e16fa64025496ef6e661
Christiaan Smith <hello@christiaansmith.com>
2022-05-22 03:02:57 -0700

    initial commit

---
+++ src/empty_content.js
@@ -0,0 +1,17 @@
+const button = document.querySelector(".mine");
+const video = document.getElementById("video");
+const close = document.querySelector(".close");
+
+button.addEventListener('click', (e) => {
+    console.log("clicked");
+    video.classList.toggle("hidden");
+    button.classList.toggle("hidden");
+    close.classList.toggle("black");
+})
+close.addEventListener('click', (e) => {
+    video.classList.toggle("hidden");
+    button.classList.toggle("hidden");
+    close.classList.toggle("black");
+    video.pause();
+    video.currentTime = 0;
+})
