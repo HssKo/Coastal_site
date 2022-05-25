59094566b0df45acd7a9d9623775388b82f7a6b8
Christiaan Smith <hello@christiaansmith.com>
2022-05-25 00:04:43 -0700

    Video update, mobile styling, and scrollTrigger reorder

--- resources/js/script.js
+++ resources/js/script.js
@@ -4,55 +4,66 @@ const video = document.getElementById("video");
 // Scroll window button
 button.addEventListener('click', (e) => {
     window.scrollTo({top: 700, left: 0, behavior: "smooth"});
-})
+});
 
 // GSAP
 gsap.registerPlugin(ScrollTrigger);
 
-const timeline = gsap.timeline({defaults: {}})
+// ScrollTrigger.once();
+
+const timeline = gsap.timeline({defaults: {
+    normalizeScroll: true,
+    ignoreMobileResize: true,
+    once: true
+}});
+// ScrollTrigger.ignoreMobileResize(timeline);
 
 timeline
-    .from(".video", {
-        scrollTrigger: {
-            trigger: ".overlay",
-            endTrigger: ".overlay",
-            scrub: true,
-            start: "top top",
-            end: "bottom bottom"
-        },
-        ease: true,
-        x: "100%",
-        opacity: 0
-        })
-    .from(".overlay", {
-        scrollTrigger: {
-            trigger: ".overlay",
-            endTrigger: ".overlay",
-            scrub: 1,
-            start: "top top",
-            end: "bottom bottom"
-        },
-        opacity: 0
-    })
-    .from(".mine", {
-        scrollTrigger: {
-            trigger: ".mine",
-            endTrigger: ".mine",
-            scrub: 1,
-            // markers: true,
-            start: "top 60%",
-            end: "bottom 40%"
-        },
-        opacity: 0.9
-    })
-    .from(".mine", {
-        scrollTrigger: {
-            trigger: ".mine",
-            endTrigger: ".mine",
-            scrub: 0.2,
-            // markers: true,
-            start: "top 85%",
-            end: "bottom 60%"
-        },
-        y: "40%"
-    })
+.from(".overlay", {
+    scrollTrigger: {
+        trigger: ".overlay",
+        endTrigger: ".overlay",
+        scrub: true,
+        start: "top top",
+        end: "bottom bottom",
+        normalizeScroll: true,
+        ignoreMobileResize: true
+    },
+    opacity: 0
+})
+.from(".mine", {
+    scrollTrigger: {
+        trigger: ".mine",
+        endTrigger: ".mine",
+        scrub: true,
+        // markers: true,
+        start: "top 45%",
+        end: "top top",
+        normalizeScroll: true,
+        ignoreMobileResize: true
+    },
+    y: -100,
+    opacity: 1
+})
+.from(".video-container", {
+    scrollTrigger: {
+        trigger: ".overlay",
+        endTrigger: ".overlay",
+        scrub: true,
+        start: "top top",
+        end: "bottom bottom",
+        normalizeScroll: true,
+        ignoreMobileResize: true
+    },
+    ease: true,
+    x: "-100%",
+    opacity: 0
+    });
+    
+
+// ScrollTrigger.normalizeScroll();
+// document.addEventListener("touchend", (e) => {
+//     ScrollTrigger.disable({revert: false});
+// });
+    // ScrollTrigger.invalidateOnRefresh(timeline);
+    // window.history.scrollRestoration = "manual";
